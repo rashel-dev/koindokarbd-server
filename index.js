9acb386210977044bfbf63ff4d722d3296a42625
rashel9255 <ibrahim2000167@gmail.com>
2026-01-10 04:38:06 +0600

    create api to get all users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import cors from "cors";
+const port = process.env.PORT || 3000;
 
 dotenv.config();
 
@@ -9,13 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+//mysql connection
 const db = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "REMOVED",
-    // database:"",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
 });
 
-console.log("mysql server has been connected");
+app.get("/", (req, res) => {
+    res.send("Welcome to Koindokarbd Server");
+});
 
+app.get("/users", async (req, res) => {
+    const [rows] = await db.query("SELECT * FROM users");
+    res.json(rows);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
